0feb0baa4f6497b6c010e392fd2fc415393e7855
rwoo <wooheaven@naver.com>
2017-01-18 02:58:45 +0900

    modify class

--- src/test/java/rwoo/research/ArrayCopyTest.java
+++ src/test/java/rwoo/research/ArrayCopyTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -13,12 +14,37 @@ public class ArrayCopyTest {
     @Test
     public void testArrayCopy() {
         List<String> listA = new ArrayList<String>();
-        String[] tmpA = {"a", "b", "c"};
+        String[] tmpA = {"a", "b"};
         listA = Arrays.asList(tmpA);
         List<String> expextA = new ArrayList<String>();
         expextA.add("a");
         expextA.add("b");
-        expextA.add("c");
         assertThat("listA is different", listA, is(expextA));
+
+        List<String> listB = new ArrayList<String>();
+        String[] tmpB = {"c", "d"};
+        listB = Arrays.asList(tmpB);
+        List<String> expextB = new ArrayList<String>();
+        expextB.add("c");
+        expextB.add("d");
+        assertThat("listB is different", listB, is(expextB));
+
+//        listA.addAll(listB); java.lang.UnsupportedOperationException
+
+        List<String> listC = new ArrayList<String>();
+        listC.addAll(listA);
+        listC.addAll(listB);
+        List<String> expextC = new ArrayList<String>();
+        expextC.add("a");
+        expextC.add("b");
+        expextC.add("c");
+        expextC.add("d");
+        assertThat("listC is different", listC, is(expextC));
+
+        List<String> listD = new ArrayList<String>();
+        listD.add("a");
+        listD.add("b");
+        listD.addAll(listB);
+        assertThat("listD is different", listD, is(expextC));
     }
 }
