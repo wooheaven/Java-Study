657d171d5e30c9db251949c5bdd1bd00e220354d
rwoo <wooheaven79@gmail.com>
2017-10-11 02:06:26 +0900

    #15-junit-test-code-private-method modified:   .gitignore modified:   01_JDK7/src/main/java/rwoo/research/MockPrivateMethod.java modified:   01_JDK7/src/test/java/rwoo/research/MockPrivateMethodTest.java

--- 01_JDK7/src/main/java/rwoo/research/MockPrivateMethod.java
+++ 01_JDK7/src/main/java/rwoo/research/MockPrivateMethod.java
@@ -1,13 +1,11 @@
 package rwoo.research;
 
-import java.util.Date;
-
 public class MockPrivateMethod {
     public String getDetails() {
         return "Mock private method example: " + iAmPrivate();
     }
 
     private String iAmPrivate() {
-        return new Date().toString();
+        return "iAmPrivateReturn";
     }
 }
